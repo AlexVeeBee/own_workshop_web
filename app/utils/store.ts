7a4ea3f97525d199b24dc8a04708620549dc9d48
alexveebee <alex100.askels@gmail.com>
2024-07-05 20:56:06 +0000

    Remove unused interface

--- app/utils/store.ts
+++ app/utils/store.ts
@@ -2,12 +2,6 @@ import instanceStore from "./store/instance";
 import userStore from "./store/user";
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-interface instacne {
-    banner: string | null;
-    title: string;
-    description: string;
-}
-
 const store = configureStore({
     reducer: {
         instance: instanceStore,
